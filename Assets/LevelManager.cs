0b4ef7818ce274e6000f70a6c91e6ea78dbeab03
Dustin Hudyma <dhudyma@gmail.com>
2017-03-06 13:30:42 -0800

    Added buttons to move through Scenes in the correct order.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,6 +5,7 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		Debug.Log ("Load level requested: " + name);
+		Application.LoadLevel (name);
 	}
 	
 	public void QuitRequest () {
